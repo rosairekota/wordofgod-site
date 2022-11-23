d1a40a09f5933270d3461b13e6a5dbffc86067f0
Essonga Godson <peyagodson@ymail.com>
2022-11-23 22:43:40 +0300

    project initialized and tailwind added

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,17 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-    return (
-        <div className="h-screen">
-            <div className="flex w-screen h-1/2">
-                <div className="w-1/2  h-full bg bg-green-700"/>
-                <div className="w-1/2  h-full bg bg-yellow-700"/>
-            </div>
-            <div className="flex  h-1/2">
-                <div className="w-1/3 h-full bg-red-700"/>
-                <div className="w-2/3  h-full bg bg-orange-700"/>
-                <div className="w-1/3  h-full bg bg-pink-700"/>
-            </div>
-        </div>
-    )
+  return (
+    <div className="h-screen">
+      <div className="flex w-screen h-1/2">
+        <div className="w-1/2 h-full bg bg-green-700"/>
+        <div className="w-1/2 h-full bg bg-yellow-700"/>
+      </div>
+      <div className="flex h-1/2">
+        <div className="w-1/3 h-full bg-red-700"/>
+        <div className="w-2/3 h-full bg bg-orange-700"/>
+        <div className="w-1/3 h-full bg bg-pink-700"/>
+      </div>
+    </div>
+  )
 }
