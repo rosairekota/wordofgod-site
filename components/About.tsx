5134b64ae6cc028bc5fced7e7768f9921f1ee8d3
rkota(Rosaire Kota) <rosairekota@gmail.com>
2022-12-18 22:08:06 +0100

    feat: add about section

---
+++ components/About.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import React from 'react'
+import aboutImage from "../assets/images/about_image.png";
+import TitleSection from '@/components/shared/TitleSection';
+interface Props {
+    
+}
+
+const About = (props: Props) => {
+    return (
+        <div className="h-auto w-full primary-light my-20">
+        <div className="container px-5 mx-auto grid md:grid-cols-2 gap-10">
+        <div>
+          {/* <div className="h-28 w-0.5 bg-orange-300/90 absolute left-40"></div>
+          <div className="h-0.5 w-28 bg-orange-300/90 absolute left-40 bottom-0 top-0"></div> */}
+           <TitleSection subTitle="About" title={""}/>
+           <p className='mb-4'>And yet, in spite of everything, Christ promised that His Holy Spirit would remain in the Church until the end of the world. This is the central thought and basis of our services.</p>
+           <p>And yet, in spite of everything, Christ promised that His Holy Spirit would remain in the Church until the end of the world. This is the central thought and basis of our services.</p>
+         </div>
+         <div>
+         <Image src={aboutImage} alt="test" className="cover" />
+         </div>
+        </div>
+       </div>
+    )
+}
+
+export default About
