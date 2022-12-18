bd1a4599287e6d5d444d8ded797c0152c6095c4d
rkota(Rosaire Kota) <rosairekota@gmail.com>
2022-12-18 22:10:35 +0100

    feat: add vision section

---
+++ components/Vision.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import TitleSection from "@/components/shared/TitleSection";
+import journalCard from "@/assets/images/journalCard.png";
+import JournalCard from "@/components/shared/JournalCard";
+import { IJournal } from "types";
+interface Props {}
+
+const Vision = (props: Props) => {
+  return (
+    <div className="h-96 w-full bg-secondary text-white">
+      <div className="container px-5 mx-auto py-10">
+        <div className="flex justify-center items-center">
+          <TitleSection subTitle="Journal" title={""} />
+        </div>
+
+        <div className="grid md:grid-cols-3 2xl:grid-cols-4 gap-4">
+        </div>
+        <div className="flex justify-center items-center">
+          <button className="bg-secondary py-3 px-10 my-5 text-white rounded-full hover:bg-primary/90 mx-3">
+            Read more
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Vision;
