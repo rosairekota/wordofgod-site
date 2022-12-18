5fff2ef859788ad51499c4542d95036f154d3b3d
rkota(Rosaire Kota) <rosairekota@gmail.com>
2022-12-18 22:12:54 +0100

    feat: include any sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+import {Banner} from "@/components/Banner";
+import { Navbar } from "@/layouts/Navbar";
+import About from "@/components/About";
+import Journal from "@/components/Journal";
+import Vision from "@/components/Vision";
 
-export default function Home() {
-    return (
-        <div className="h-screen">
-            <div className="flex w-screen h-1/2">
-                <div className="w-1/2  h-full bg bg-green-700"/>
-                <div className="w-1/2  h-full bg bg-yellow-700"/>
-            </div>
-            <div className="flex  h-1/2">
-                <div className="w-1/3 h-full bg-red-700"/>
-                <div className="w-2/3  h-full bg bg-orange-700"/>
-                <div className="w-1/3  h-full bg bg-pink-700"/>
-            </div>
-        </div>
-    )
+type Props = {
+  imageUrl: ImageUrl;
+  imageAlt: string;
+  title: string;
+  message: string;
+};
+type ImageUrl = {
+  src: string;
+};
+export default function Home({ imageUrl, imageAlt, title, message }: Props) {
+  return (
+    <div>
+      <Banner/>
+      <About/>
+      <Journal/>
+      <Vision/>
+    </div>
+  );
 }
