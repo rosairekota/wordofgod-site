9cc17294424ca7610fd48d7a7d3261725a0527cd
rkota(Rosaire Kota) <rosairekota@gmail.com>
2022-12-19 04:10:32 +0100

    refactor: update style

--- components/shared/JournalCard.tsx
+++ components/shared/JournalCard.tsx
@@ -9,11 +9,11 @@ const JournalCard:React.FC<Props> = ({imageUrl,journalDate,createdBy,title,descr
     <div className="bg-white drop-shadow-xl">
       <Image src={imageUrl} alt="test" className="cover" />
       <div>
-        <div className="flex justify-between py-3 mx-3">
+        <div className="flex justify-between py-3 mx-3 text-stone-400">
           <span>{journalDate}</span>
           <span>.by Pastor</span>
         </div>
-        <p className="pb-10 mx-3">
+        <p className="pb-10 mx-3 text-stone-900 font-medium">
          {description}
          </p>
         <button className="bg-primary py-3 px-10 my-5 text-white rounded-full hover:bg-primary/90 mx-3">
